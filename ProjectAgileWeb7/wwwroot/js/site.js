e42ccb79a2c062539d93555b6f7017d5ac673efc
Andreea Nenciu <andreeanen@yahoo.com>
2020-04-28 18:04:52 +0200

    WEB7-107 , WEB-166 Implement weather API and add date

--- ProjectAgileWeb7/wwwroot/js/site.js
+++ ProjectAgileWeb7/wwwroot/js/site.js
@@ -58,9 +58,101 @@ $(document).ready(function () {
     }).val('');
 });
 
+
+const kToC = function (kelvin) {
+
+    return Math.round(kelvin - 273.15);
+}
+
+const getToday = function () {
+    let today = new Date();
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    const weekdaysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday"];
+
+    let myWeekday = weekdaysName[today.getDay()];
+    let date = today.getDate() + ' ' + monthNames[today.getMonth()] + ' ' + today.getFullYear()
+
+    return myWeekday + ' ' + date;
+}
+
+const getTimeNow = function () {
+    var today = new Date();
+    return today.getHours() + ":" + today.getMinutes();
+}
+
+const getWeather = function () {
+    const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?';
+    const apiKey = '2fa2b9559079337382b2c8d5b4817940';
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+    let queryString = `lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    let endpoint = baseUrl + queryString;
+    const temperature = document.getElementById('temperature');
+    const weather = document.getElementById('weather');
+    const location = document.getElementById('location');
+    const today = document.getElementById('today');
+    const time = document.getElementById('time');
+    const description = document.getElementById('description');
+    const weatherIcon = document.getElementById('weather-icon');
+    const wind = document.getElementById('wind');
+
+
+    fetch(endpoint)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            if (data.cod === 200) {
+
+                switch (data.weather[0].main) {
+                    case 'Rain':
+                        weatherIcon.innerHTML = '<i class="fas fa-cloud-rain fa-2x text-info"></i>';
+                        break;
+                    case 'Clouds':
+                        weatherIcon.innerHTML = '<i class="fas fa-cloud fa-2x text-secondary"></i>';
+                        break;
+                    case 'Clear':
+                        weatherIcon.innerHTML = '<i class="fas fa-sun fa-2x text-warning"></i>';
+                        break;
+                    case 'Snow':
+                        weatherIcon.innerHTML = '<i class="fas fa-snowflake fa-2x text-info"></i>';
+                        break;
+                    default:
+                        weatherIcon.innerHTML = ' <i class="fas fa-cloud-sun fa-2x text-primary"></i>';
+                }
+
+                location.innerHTML = data.name;
+                weather.innerHTML = data.weather[0].main;
+                description.innerHTML = data.weather[0].description;
+                temperature.innerHTML = kToC(data.main.temp) + '°C';
+                wind.innerHTML = data.wind.speed;
+                today.innerHTML = getToday();
+                time.innerHTML = getTimeNow();
+            }
+            else {
+                weather.innerHtml = 'No weather data available';
+            }
+        })
+        .catch(function (error) {
+            return console.log(error);
+        });
+
+
+}
+
+
+
 $(document).ready(function () {
     $('#submit-btn').attr('disabled', true);
     $('#search-input').keyup(function () {
         ($(this).val().length == 0) ? $('#submit-btn').attr('disabled', true) : $('#submit-btn').attr('disabled', false);
-    })
+    });
+
+    //$('#my-testing-button').click(function () {
+    //    getWeather();
+    //});
+    getWeather();
 });
