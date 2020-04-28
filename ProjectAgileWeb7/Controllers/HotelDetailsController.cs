c84e68ef13444f875ebad64efd9bb6fd573fae6e
175247 <yarr-tm@hotmail.com>
2020-04-28 21:56:52 +0200

    Update to the booking page.

--- ProjectAgileWeb7/Controllers/HotelDetailsController.cs
+++ ProjectAgileWeb7/Controllers/HotelDetailsController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,11 +16,22 @@ namespace ProjectAgileWeb7.Controllers
     public class HotelDetailsController : Controller
     {
         private readonly ApplicationDbContext _appContext;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HotelDetailsController(ApplicationDbContext appContext)
+        public HotelDetailsController(ApplicationDbContext appContext,
+            UserManager<ApplicationUser> userManager)
         {
             _appContext = appContext;
+            _userManager = userManager;
         }
+
+        [BindProperty]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [BindProperty]
+        public ApplicationUser CurrentUser { get; set; }
 
         public async Task<IActionResult> HotelPage(int id)
         {
@@ -26,16 +40,20 @@ namespace ProjectAgileWeb7.Controllers
                 .Include(r => r.Rooms)
                 .Include(f => f.HotelFacilities)
                 .ThenInclude(fa => fa.Facility)
-                .ToListAsync();            
+                .ToListAsync();
 
             return View(hotel[0]);
         }
 
-        public async Task<IActionResult> BookRoom(RoomType roomType)
+        public async Task<IActionResult> BookRoom(RoomType roomType, int id)
         {
             var rooms = await _appContext.Rooms
-                .Where(t => t.RoomType == roomType)
+                .Where(t => t.RoomType == roomType).Include(h => h.Hotel).Where(hi => hi.HotelId == id)
                 .ToListAsync();
+
+            CurrentUser = await _userManager.GetUserAsync(User);
+            if (CurrentUser != null)
+                ViewData["User"] = CurrentUser;
 
             return View(rooms[0]);
         }
