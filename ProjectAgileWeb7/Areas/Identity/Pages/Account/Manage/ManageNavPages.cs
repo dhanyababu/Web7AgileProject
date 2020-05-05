700d25c670d5cf568ccf6a434f3772d001f57723
Andreea Nenciu <andreeanen@yahoo.com>
2020-05-05 08:43:25 +0200

    WEB7-95 Create view for my bookings feature

--- ProjectAgileWeb7/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ ProjectAgileWeb7/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace ProjectAgileWeb7.Areas.Identity.Pages.Account.Manage
 {
@@ -31,6 +28,8 @@ namespace ProjectAgileWeb7.Areas.Identity.Pages.Account.Manage
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
 
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+
+
 
         private static string PageNavClass(ViewContext viewContext, string page)
         {
