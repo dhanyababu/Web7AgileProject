5a85414d87dfcf001a026d95bc54bd7e3d538393
Tijana <tijana.ilic87@protonmail.com>
2020-05-15 14:01:27 +0200

    WEB7-277 Add [Required] validation to room properties

--- ProjectAgileWeb7/Models/Room.cs
+++ ProjectAgileWeb7/Models/Room.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjectAgileWeb7.Models
 {
@@ -6,13 +7,25 @@ namespace ProjectAgileWeb7.Models
     public class Room
     {
         public int RoomId { get; set; }
+
+        [Required]
         public RoomType RoomType { get; set; }
+
+        [Required]
         [Column(TypeName = "decimal(18,4)")]
         public decimal RoomPrice { get; set; }
+
+        [Required]
         public string RoomDescription { get; set; }
+
+        [Required]
         public int NumberOfBeds { get; set; }
+
+        [Required]
         public int Capacity { get; set; } //how many persons
         public string RoomImageUrl { get; set; }
+
+        [Required]
         public int HotelId { get; set; }
         public Hotel Hotel { get; set; }
     }
