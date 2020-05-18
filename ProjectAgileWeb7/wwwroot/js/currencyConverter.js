aa201dc6ddd07e0fb78d2f962a778389d6f0bea7
175247 <yarr-tm@hotmail.com>
2020-05-18 14:14:58 +0200

    WEB7-284 Bug issue fix. Issue Closed.

--- ProjectAgileWeb7/wwwroot/js/currencyConverter.js
+++ ProjectAgileWeb7/wwwroot/js/currencyConverter.js
@@ -1,4 +1,6 @@
-﻿let baseCurrency;
+﻿let initialPricePerNight;
+let initialTotalPrice;
+let baseCurrency;
 let amountPerNightToConvert;
 let amountTotalToConvert;
 let selectedCurrency;
@@ -8,32 +10,29 @@ let newTotalAmount;
 let ajaxResult;
 
 $(document).ready(function () {
+    initialPricePerNight = $("#initialPricePerNight").val();
+    initialTotalPrice = $("#initialTotalPrice").val();
+
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
         $(".totalPriceHidden").val($(".totalPriceDisplayLabel").text());
     })
 
-    $(".selectedCurrency").focus(function () {
-        baseCurrency = $(this).val();
-    });
-
     $(".selectedCurrency").on('change', function () {
         selectedCurrency = $(".selectedCurrency option:selected").text();
-        amountPerNightToConvert = $(".perNightPriceDisplay").text();
-        amountTotalToConvert = $(".totalPriceDisplayLabel").text();
 
         $(".selectedCurrency").blur();
 
         $.ajax({
-            url: "https://prime.exchangerate-api.com/v5/a5f16bf9860bddcdd3cf4c43/latest/" + baseCurrency,
+            url: "https://prime.exchangerate-api.com/v5/a5f16bf9860bddcdd3cf4c43/latest/SEK/",
             type: "GET",
             dataType: "json",
             success: function (response) {
                 ajaxResult = response.conversion_rates;
-                exchangeRate = parseFloat(ajaxResult[selectedCurrency]);
+                exchangeRate = ajaxResult[selectedCurrency];
 
-                newPerNightAmount = (amountPerNightToConvert * exchangeRate).toFixed(2);
-                newTotalAmount = (amountTotalToConvert * exchangeRate).toFixed(2);
+                newPerNightAmount = parseFloat(initialPricePerNight * exchangeRate).toFixed(2);
+                newTotalAmount = parseFloat(initialTotalPrice * exchangeRate).toFixed(2);
                 $(".currentCurrency").text(selectedCurrency);
                 $(".perNightPriceDisplay").text(newPerNightAmount);
                 $(".totalPriceDisplayLabel").text(newTotalAmount);
