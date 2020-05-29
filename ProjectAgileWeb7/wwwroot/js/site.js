0bc5aac18537f641cb298a43ca3f6f6e79d2e48d
Andreea Nenciu <andreeanen@yahoo.com>
2020-05-29 10:58:41 +0200

    WEB7-294 Change getNearbyPlaces to fix errors

--- ProjectAgileWeb7/wwwroot/js/site.js
+++ ProjectAgileWeb7/wwwroot/js/site.js
@@ -179,19 +179,9 @@ const getWeather = function () {
                 return console.log(error);
             });
     }
-
-
-
 }
 
 $(document).ready(function () {
-    //if ($('#search-input').val().length == 0) {
-    //    $('#submit-btn').attr('disabled', true);
-    //}
-    //$('#search-input').keyup(function () {
-    //    ($(this).val().length == 0) ? $('#submit-btn').attr('disabled', true) : $('#submit-btn').attr('disabled', false);
-    //});
-
     getWeather();
     getPlacesNearby();
 });
@@ -199,45 +189,62 @@ $(document).ready(function () {
 const getPlacesNearby = function () {
     const baseUrlPath = "https://places.ls.hereapi.com/places/v1/discover/explore?";
     const apiKey = "CIFDa7Y1OWPrVKL-VUgnzaSZ01kCdQ553yn8HZaiyfc";
-    const latitude = document.getElementById('latitude').value;
-    const longitude = document.getElementById('longitude').value;
-    let queryString = `at=${latitude},${longitude}`;
+    const latitude = document.getElementById('latitude');
+    const longitude = document.getElementById('longitude');
+    if (latitude != null) {
+        latitudeValue = latitude.value;
+    }
+    else {
+        latitudeValue = null;
+    }
+
+    if (longitude != null) {
+        longitudeValue = longitude.value;
+    }
+    else {
+        longitudeValue = null;
+    }
+    let queryString = `at=${latitudeValue},${longitudeValue}`;
     let endpoint = baseUrlPath + "apiKey=" + apiKey + "&" + queryString;
 
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            let restaurantsCount = 0;
-            let barsCount = 0;
-            let cultureCount = 0;
-            let shopsCount = 0;
-            data.results.items.forEach(item => {
-                if (item.category.id === "restaurant" || item.category.id === "food-drink") {
-                    restaurantsCount++;
-                    document.getElementById('restaurantsCount').innerHTML = restaurantsCount;
-                    document.getElementById('restaurants').innerHTML += "<details><summary><i class=\"fas fa-chevron-circle-down fa-sm\"></i> &nbsp;" + item.title + "</summary><span class=\"small text-muted text-capitalize\"><span class=\"text-primary\">Type: </span>" + item.category.id + " (" + item.tags[0].id + ")</br><span class=\"text-primary\">Distance: </span>" + item.distance + " m</br><span class=\"text-primary\">Address:</span></br>" + item.vicinity + "<hr>";
-                }
-                else if (item.category.id === "bar-pub" || item.category.id === "coffee-tea" || item.category.id === "dance-night-club" || item.category.id === "going-out") {
-                    barsCount++;
-                    document.getElementById('barsCount').innerHTML = barsCount;
-                    document.getElementById('bars').innerHTML += "<details><summary><i class=\"fas fa-chevron-circle-down fa-sm\"></i> &nbsp;" + item.title + "</summary><span class=\"small text-muted text-capitalize\"><span class=\"text-primary\">Type: </span>" + item.category.id + "</br><span class=\"text-primary\">Distance: </span>" + item.distance + " m</br><span class=\"text-primary\">Address:</span></br>" + item.vicinity + "<hr>";
-                }
-                else if (item.category.id === "theatre-music-culture" || item.category.id === "sights-museums") {
-                    cultureCount++;
-                    document.getElementById('cultureCount').innerHTML = cultureCount;
-                    document.getElementById('culture').innerHTML += "<details><summary><i class=\"fas fa-chevron-circle-down fa-sm\"></i> &nbsp;" + item.title + "</summary><span class=\"small text-muted text-capitalize\"><span class=\"text-primary\">Type: </span>" + item.category.id + "</br><span class=\"text-primary\">Distance: </span>" + item.distance + " m</br><span class=\"text-primary\">Address:</span></br>" + item.vicinity + "<hr>";
-                }
-                else if (item.category.id === "shop" || item.category.id === "bookshop" || item.category.id === "clothing-accessories-shop" || item.category.id === "department-store") {
-                    shopsCount++;
-                    document.getElementById('shopsCount').innerHTML = shopsCount;
-                    document.getElementById('shopping').innerHTML += "<details><summary><i class=\"fas fa-chevron-circle-down fa-sm\"></i> &nbsp;" + item.title + "</summary><span class=\"small text-muted text-capitalize\"><span class=\"text-primary\">Type: </span>" + item.category.id + "</br><span class=\"text-primary\">Distance: </span>" + item.distance + " m</br><span class=\"text-primary\">Address:</span></br>" + item.vicinity + "<hr>";
-                }
-                console.log(item.category.id);
+    if (latitudeValue != null && longitudeValue != null) {
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(data => {
+                //console.log(data);
+                let restaurantsCount = 0;
+                let barsCount = 0;
+                let cultureCount = 0;
+                let shopsCount = 0;
+                data.results.items.forEach(item => {
+                    if (item.category.id === "restaurant" || item.category.id === "food-drink") {
+                        restaurantsCount++;
+                        document.getElementById('restaurantsCount').innerHTML = restaurantsCount;
+                        document.getElementById('restaurants').innerHTML += "<details><summary><i class=\"fas fa-chevron-circle-down fa-sm\"></i> &nbsp;" + item.title + "</summary><span class=\"small text-muted text-capitalize\"><span class=\"text-primary\">Type: </span>" + item.category.id + " (" + item.tags[0].id + ")</br><span class=\"text-primary\">Distance: </span>" + item.distance + " m</br><span class=\"text-primary\">Address:</span></br>" + item.vicinity + "<hr>";
+                    }
+                    else if (item.category.id === "bar-pub" || item.category.id === "coffee-tea" || item.category.id === "dance-night-club" || item.category.id === "going-out") {
+                        barsCount++;
+                        document.getElementById('barsCount').innerHTML = barsCount;
+                        document.getElementById('bars').innerHTML += "<details><summary><i class=\"fas fa-chevron-circle-down fa-sm\"></i> &nbsp;" + item.title + "</summary><span class=\"small text-muted text-capitalize\"><span class=\"text-primary\">Type: </span>" + item.category.id + "</br><span class=\"text-primary\">Distance: </span>" + item.distance + " m</br><span class=\"text-primary\">Address:</span></br>" + item.vicinity + "<hr>";
+                    }
+                    else if (item.category.id === "theatre-music-culture" || item.category.id === "sights-museums") {
+                        cultureCount++;
+                        document.getElementById('cultureCount').innerHTML = cultureCount;
+                        document.getElementById('culture').innerHTML += "<details><summary><i class=\"fas fa-chevron-circle-down fa-sm\"></i> &nbsp;" + item.title + "</summary><span class=\"small text-muted text-capitalize\"><span class=\"text-primary\">Type: </span>" + item.category.id + "</br><span class=\"text-primary\">Distance: </span>" + item.distance + " m</br><span class=\"text-primary\">Address:</span></br>" + item.vicinity + "<hr>";
+                    }
+                    else if (item.category.id === "shop" || item.category.id === "bookshop" || item.category.id === "clothing-accessories-shop" || item.category.id === "department-store") {
+                        shopsCount++;
+                        document.getElementById('shopsCount').innerHTML = shopsCount;
+                        document.getElementById('shopping').innerHTML += "<details><summary><i class=\"fas fa-chevron-circle-down fa-sm\"></i> &nbsp;" + item.title + "</summary><span class=\"small text-muted text-capitalize\"><span class=\"text-primary\">Type: </span>" + item.category.id + "</br><span class=\"text-primary\">Distance: </span>" + item.distance + " m</br><span class=\"text-primary\">Address:</span></br>" + item.vicinity + "<hr>";
+                    }
+                    //console.log(item.category.id);
+                });
+            })
+            .catch(function (error) {
+                return console.log(error);
             });
-        })
-        .catch(function (error) {
-            return console.log(error);
-        });
+    }
+
+
 
 }
