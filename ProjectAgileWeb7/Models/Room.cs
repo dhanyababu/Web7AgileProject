d355ed8ea88db3f0387d317cec70f7a68c5ce888
Tijana <tijana.ilic87@protonmail.com>
2020-05-29 13:59:47 +0200

    Add data validation to Room model

--- ProjectAgileWeb7/Models/Room.cs
+++ ProjectAgileWeb7/Models/Room.cs
@@ -9,19 +9,27 @@ namespace ProjectAgileWeb7.Models
         public int RoomId { get; set; }
 
         [Required]
+        [Display(Name = "Room Type")]
         public RoomType RoomType { get; set; }
 
         [Required]
         [Column(TypeName = "decimal(18,4)")]
+        [Range(0, double.PositiveInfinity)]
+        [Display(Name = "Room Price")]
         public decimal RoomPrice { get; set; }
 
         [Required]
+        [StringLength(255)]
+        [Display(Name = "Room Description (max 255 characters)")]
         public string RoomDescription { get; set; }
 
         [Required]
+        [Display(Name = "Number of Beds")]
+        [Range(1, 5)]
         public int NumberOfBeds { get; set; }
 
         [Required]
+        [Range(1, 7)]
         public int Capacity { get; set; } //how many persons
         public string RoomImageUrl { get; set; }
 
